28e115954bfcb4a800ae1ea319294ef25b4a3dde
Cam Prier <CSPrier@gmail.com>
2019-01-21 15:51:47 -0500

    Added some styles and am deploying with netlify

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>ReactJS Calculator</h1>
-        </header>
         <main>
           <Calculator />
         </main>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -57,30 +57,28 @@ class Calculator extends Component {
       <div className="calculator-container">
         <div className="calculator">
           <Display data={this.state.operations} />
-          <div className="operators">
-            <Button onClick={this.handleClick} label="-" value="-" />
-            <Button onClick={this.handleClick} label="+" size="2" value="+" />
-            <Button onClick={this.handleClick} label="/" value="/" />
-            <Button onClick={this.handleClick} label="x" value="*" />
-            <Button onClick={this.handleClick} label="." value="." />
-            <Button onClick={this.handleClick} label="C" value="clear" />
-            <Button onClick={this.handleClick} label="=" size="2" value="equal" />
-          </div>
+          <div className="buttons">
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
 
-          <div className="numbers">
-            <Button onClick={this.handleClick} label="7" value="7" />
-            <Button onClick={this.handleClick} label="4" value="4" />
-            <Button onClick={this.handleClick} label="1" value="1" />
-            <Button onClick={this.handleClick} label="0" value="0" />
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
 
-            <Button onClick={this.handleClick} label="8" value="8" />
-            <Button onClick={this.handleClick} label="5" value="5" />
-            <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
+          <Button label="" value="null" />
 
-            <Button onClick={this.handleClick} label="9" value="9" />
-            <Button onClick={this.handleClick} label="6" value="6" />
-            <Button onClick={this.handleClick} label="3" value="3" />
-            <Button label="" value="null" />
+          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="+" size="2" value="+" />
+          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
           </div>
         </div>
       </div>
